8c4030d7c0a06084726b2bc460a95e6a43198d06
Alexis LUCIEN <alexis-externe.lucien@enedis.fr>
2024-02-07 10:58:20 +0100

    first implementation of bitsize

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import copy
 import cv2 as cv
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw
 
 def biter(b: int, step = 1):
     """
@@ -46,26 +46,35 @@ class BitEncoding(Encoding):
     """
     Encode/decode byte data to image where 0 is black and 1 is white (binary).
     """
+    BITSIZE = (4, 4)
+    
     @staticmethod
     def encode(data: bytes, image_size: tuple[int, int], output_path: str):
+        # check if image_size is a multiple of BITSIZE
+        if image_size[0] % BitEncoding.BITSIZE[0] != 0 or image_size[1] % BitEncoding.BITSIZE[1] != 0:
+            raise ValueError(f'image_size ({image_size}) must be a multiple of {BitEncoding.BITSIZE}')
+        
         im = Image.new('1', image_size)
+        draw = ImageDraw.Draw(im)
     
+        # TODO: use numpy manipulation instead of naive rectangle
         i, x, y = 0, 0, 0
+        dx, dy = BitEncoding.BITSIZE
         for byte in data:
             for bit in reversed(list(biter(byte))):
-                im.putpixel((x, y), bit)
-                
-                x += 1
+                draw.rectangle([(x, y), (x + dx, y + dy)], fill=bit)
+                x += dx
                 
                 # next row
                 if x >= image_size[0]:
                     x = 0
-                    y += 1
+                    y += dy
                 
                 # next image
                 if y >= image_size[1]:
                     im.save(output_path.replace('%num%', str(i)))
                     im = Image.new('1', image_size)
+                    draw = ImageDraw.Draw(im)
                     x, y = 0, 0
                     i += 1
         
@@ -74,6 +83,8 @@ class BitEncoding(Encoding):
     @staticmethod
     def decode(path: str) -> bytes:
         im = Image.open(path)
+        # TODO: use numpy manipulation instead of naive image resize
+        im = im.resize((im.size[0] // BitEncoding.BITSIZE[0], im.size[1] // BitEncoding.BITSIZE[1]))
         
         out = bytearray()
         data = list(im.getdata())
