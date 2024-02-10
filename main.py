f4e1cddef6c2d11c752c6fd964c52bd1b4be4910
Alexis LUCIEN <alexis-externe.lucien@enedis.fr>
2024-02-10 22:21:31 +0100

    bunch of optimisations

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import os
+import re
+import sys
 import glob
 import math
 import numpy as np
 from PIL import Image
 from utils import BitEncoding
+
+np.set_printoptions(threshold=sys.maxsize)
+
+# TODO: implement image scaling
+# DONE: works with different image sizes (whatever ratio)
 
 image_scale = 1
 image_size = (10, 10)
@@ -12,11 +19,38 @@ image_length = image_size[0] * image_size[1]
 
 
 def numpy_zoom(array: np.ndarray, factor: int) -> np.ndarray:
+    """
+    Zoom an array.
+
+    Args:
+        array (np.ndarray): The array to zoom.
+        factor (int): The zoom factor (negative to zoom out).
+
+    Returns:
+        np.ndarray: The zoomed array.
+    """
     if factor >= 0:
         return np.repeat(np.repeat(array, factor, axis=0), factor, axis=1)
     else:
         return array[:: abs(factor), :: abs(factor)]
 
+
+def natural_order(s: str) -> int:
+    """
+    Get the natural order of a string
+
+    Args:
+        s (str): The string.
+
+    Returns:
+        int: The natural order.
+    """
+    if match := re.search(r"\d+", s):
+        return int(match.group())
+    return 0
+
+
+################################################################################
 
 # remove previous images
 for f in glob.glob("test*.png"):
@@ -26,11 +60,11 @@ for f in glob.glob("test*.png"):
 data = b"hello world"
 ndata = np.array(list(map(int, data)), dtype=np.uint8)
 ndata = np.unpackbits(ndata).reshape(-1, 8)
-ndata = numpy_zoom(ndata, image_scale).flatten()
+ndata = ndata.flatten()
 
-# split data by image_size
+# split data to chunks of image_size
 nbchunk = math.ceil(ndata.size / image_length)
-if rchunk := int((nbchunk - ndata.size / image_length) * image_length):
+if rchunk := round((nbchunk - ndata.size / image_length) * image_length):
     ndata = np.append(ndata, np.zeros(rchunk, dtype=np.uint8))
     if nbchunk != ndata.size // image_length:
         raise ValueError("Error when chunking data")
@@ -41,22 +75,22 @@ for i, ndata in enumerate(ndatas):
     ndata = ndata.reshape(image_size)
     ndata *= 255
     im = Image.fromarray(ndata, mode="L").convert("1")
-    im.save(filename.replace('%num%', str(i)), 'PNG')
+    im.save(filename.replace("%num%", str(i)), "PNG")
 
 # decode image to array
 ndatas = list()
-for f in glob.glob("test*.png"):
+for f in sorted(glob.glob("test*.png"), key=natural_order):
     im = Image.open(f)
     ndata = np.asarray(im.convert("L"))
+    ndata = ndata // 255
     ndatas.append(ndata)
 
 # decode array to byte
-data = bytearray()
-ndata = np.concatenate(*ndatas).flatten() // 255
+ndata = np.concatenate([*ndatas])
+ndata = ndata.flatten()
 ndata = np.resize(ndata, (ndata.size // 8, 8))
-for bits in ndata:
-    byte = int("".join(map(str, bits)), 2)
-    data.append(byte)
+ndata = np.packbits(ndata)
+data = ndata.tobytes()
 
 # remove endl
 data = BitEncoding.remove_endl(data)
--- utils.py
+++ utils.py
@@ -66,41 +66,41 @@ class BitEncoding(Encoding):
 
     @staticmethod
     def encode(data: bytes, image_size: tuple[int, int], output_path: str):
-        im = Image.new('1', image_size)
-    
+        im = Image.new("1", image_size)
+
         i, x, y = 0, 0, 0
         for byte in data:
             for bit in reversed(list(biter(byte))):
                 im.putpixel((x, y), bit)
-                
+
                 x += 1
-                
+
                 # next row
                 if x >= image_size[0]:
                     x = 0
                     y += 1
-                
+
                 # next image
                 if y >= image_size[1]:
-                    im.save(output_path.replace('%num%', str(i)))
-                    im = Image.new('1', image_size)
+                    im.save(output_path.replace("%num%", str(i)))
+                    im = Image.new("1", image_size)
                     x, y = 0, 0
                     i += 1
-        
-        im.save(output_path.replace('%num%', str(i)))
+
+        im.save(output_path.replace("%num%", str(i)))
 
     @staticmethod
     def decode(path: str) -> bytes:
         im = Image.open(path)
-        
+
         out = bytearray()
         data = list(im.getdata())
         for i in range(0, len(data), 8):
-            bits = data[i:i+8]
+            bits = data[i : i + 8]
             bits = [1 if b == 255 else 0 for b in bits]
-            byte = int(''.join(map(str, bits)), 2)
+            byte = int("".join(map(str, bits)), 2)
             out.append(byte)
-        
+
         return bytes(out)
 
     @staticmethod
